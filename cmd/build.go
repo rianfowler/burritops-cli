2274795757fbd02a39281a6f053d79ac254fb262
Rian Fowler <4998130+rianfowler@users.noreply.github.com>
2025-03-09 14:56:11 +0000

    add dockerfile

---
+++ cmd/build.go
@@ -0,0 +1,47 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"dagger.io/dagger"
+	"github.com/spf13/cobra"
+)
+
+// buildCmd represents the build command
+var buildCmd = &cobra.Command{
+	Use:   "build",
+	Short: "Clone code, build a container image, and publish it using Dagger",
+	Run: func(cmd *cobra.Command, args []string) {
+		ctx := context.Background()
+
+		// Connect to the Dagger engine (which leverages Docker under the hood)
+		client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
+		if err != nil {
+			log.Fatalf("failed to connect to dagger: %v", err)
+		}
+		defer client.Close()
+
+		// Clone the repository using Dagger's Git functionality.
+		repo := client.Git("https://github.com/rianfowler/burritops-cli").Branch("main")
+		// Convert the Git reference to a directory using Tree().
+		srcDir := repo.Tree()
+
+		// Create a container that mounts the directory.
+		container := client.Container().
+			From("docker.io/library/golang:1.18").
+			WithMountedDirectory("/src", srcDir).
+			WithWorkdir("/src")
+
+		// Build the image using srcDir as the build context.
+		image := container.Build(srcDir)
+
+		fmt.Println("Build successful! Image ID:", image.ID())
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(buildCmd)
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package main
+
+import "github.com/rianfowler/burritops-cli/cmd"
+
+func main() {
+	cmd.Execute()
+}
